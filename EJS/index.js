1e6daa4f117e5fec5d4a365e1cd52b1356e68243
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-01-20 13:37:31 +0545

    using ejs

--- EJS/index.js
+++ EJS/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-const port = 3000
+app.set("view engine", "ejs");
+
+// if we are starting server from outside of EJS
+app.set("views", path.join(__dirname, "/views"));
+
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+})
+
+app.get("/about", (req, res) => {
+    res.send("This is about page");
+})
+
+const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 });
