b927761c68bfeac1d212c8f3edcd50488b28ac6c
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-01-20 14:11:29 +0545

    printing value in ejs with directValue, with params, with query

--- EJS/index.js
+++ EJS/index.js
@@ -22,3 +22,23 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
+
+//  sending value to ejs file
+
+app.get("/rollDice", (req, res) => {
+    var diceVal = Math.floor(Math.random() * 6) + 1;
+    res.render("rollDice.ejs", { diceVal });
+
+    // we can also use this when have to send key value and if the key value is same then direct value
+    // res.render("rollDice.ejs", { diceVal: diceVal });
+})
+
+//  sending value to ejs file with params
+app.get("/profile/:username/:id", (req, res) => {
+    let { username, id } = req.params;
+    res.render("profile.ejs", { username, id })
+})
+app.get("/search", (req, res) => {
+    let { q } = req.query;
+    res.render("search.ejs", { q })
+})
