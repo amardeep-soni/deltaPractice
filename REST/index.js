ebdfee15aa48ff85e3cd706f9a426ff3f76912af
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-01-24 01:31:50 +0545

    get all post implementation

--- REST/index.js
+++ REST/index.js
@@ -3,16 +3,33 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.send("All thing working well")
+
+let posts = [
+    {
+        username: "amardeep",
+        content: "Learning Backend"
+    },
+    {
+        username: "amar",
+        content: "Learning Frontend"
+    },
+    {
+        username: "amar",
+        content: "Learning UI/UX"
+    }
+]
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts })
 })
+
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
