3fe8373754befd55f1583785ed1646fa63b5e4ae
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-01-24 14:11:00 +0545

    adding data in post with form

--- REST/index.js
+++ REST/index.js
@@ -11,25 +11,23 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 
-let posts = [
-    {
-        username: "amardeep",
-        content: "Learning Backend"
-    },
-    {
-        username: "amar",
-        content: "Learning Frontend"
-    },
-    {
-        username: "amar",
-        content: "Learning UI/UX"
-    }
-]
+let posts = []
 
 app.get("/posts", (req, res) => {
     res.render("index.ejs", { posts })
 })
 
+// opening form page
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+// saving form data
+app.post("/posts", (req, res) => {
+    let { username, content } = req.body;
+    posts.push({ username, content });
+    res.send("added successfully")
+})
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
