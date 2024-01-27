ced4f70380867c1ed40df57aee3fa28e96aced66
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-01-27 14:38:18 +0545

    home route make to fetch number of users

--- express_with_mysql/index.js
+++ express_with_mysql/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require('path');
 const mysql = require('mysql2');
 
 var app = express();
-const port = 3000
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -12,18 +16,25 @@ const connection = mysql.createConnection({
 })
 connection.connect();
 
-let query = "SHOW TABLES";
-try {
-    connection.query(query, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-    });
-} catch (err) {
-    console.log(err);
-}
+app.get("/", (req, res) => {
+    let query = `SELECT COUNT(*) FROM users`;
+    try {
+        connection.query(query, (err, result) => {
+            if (err) throw err;
+            console.log(result); 
+            // we get array and inside object then our count
+            let count = result[0]['COUNT(*)'];
+            res.render("home.ejs", { count });
+        });
+    } catch (err) {
+        console.log(err);
+        res.send("Something error occured in DB");
+    }
+});
 
-connection.end();
+
+// connection.end();
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 });
