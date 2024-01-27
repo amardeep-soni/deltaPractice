dd9397e659c643062a4cfc4acf5719f9b4a7c96e
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-01-27 15:46:33 +0545

    edit route to edit the username matching with password

--- express_with_mysql/index.js
+++ express_with_mysql/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 const mysql = require('mysql2');
+var methodOverride = require('method-override');
 
 var app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -21,7 +24,7 @@ app.get("/", (req, res) => {
     try {
         connection.query(query, (err, result) => {
             if (err) throw err;
-            console.log(result); 
+            console.log(result);
             // we get array and inside object then our count
             let count = result[0]['COUNT(*)'];
             res.render("home.ejs", { count });
@@ -39,6 +42,50 @@ app.get("/user", (req, res) => {
             if (err) throw err;
             let data = result;
             res.render("users.ejs", { data });
+        });
+    } catch (err) {
+        console.log(err);
+        res.send("Something error occured in DB");
+    }
+});
+
+app.get("/user/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let query = `Select  * FROM users WHERE id = '${id}'`;
+    try {
+        connection.query(query, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            res.render("edit.ejs", { user });
+        });
+    } catch (err) {
+        console.log(err);
+        res.send("Something error occured in DB");
+    }
+});
+
+app.patch("/user/:id", (req, res) => {
+    let { id } = req.params;
+    let { username, password } = req.body;
+    let query = `Select * FROM users WHERE id = '${id}'`;
+    try {
+        connection.query(query, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            if (user.password == password) {
+                query = `UPDATE users SET username = ? where id = ?`;
+                let datas = [username, id];
+                try {
+                    connection.query(query, datas, (err, result) => {
+                        if (err) throw err;
+                        res.redirect('/user');
+                    });
+                } catch (err) {
+                    res.send("Something error occured");
+                }
+            } else {
+                res.send("Password is wrong");
+            }
         });
     } catch (err) {
         console.log(err);
