4a8d9e0b5f23b16054596040506685356fc90a42
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-01-30 14:35:39 +0545

    define model and bind collection with schema

--- node_with_mongodb/index.js
+++ node_with_mongodb/index.js
@@ -7,3 +7,13 @@ main().then(() => {
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
 }
+
+// adding schema
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number,
+});
+
+// define model and bind collection with schema (creating a collction in mongodb)
+const User = mongoose.model("User", userSchema);
