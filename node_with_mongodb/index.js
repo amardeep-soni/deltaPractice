ebd004d32761f7c7f1f46f15f68b29051dce4d39
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-01-30 15:56:21 +0545

    insert multiple data at once

--- node_with_mongodb/index.js
+++ node_with_mongodb/index.js
@@ -17,3 +17,25 @@ const userSchema = new mongoose.Schema({
 
 // define model and bind collection with schema (creating a collction in mongodb)
 const User = mongoose.model("User", userSchema);
+
+// adding data to mongodb
+
+// // create instence first of user
+// const user1 = new User({
+//     name: "Bob",
+//     email: "bob@gmail.com",
+//     age: 29
+// });
+
+// // to save the data in collection
+// user1.save().then((res) => {
+//     console.log(res);
+// }).catch(err => console.log(err));
+
+// to insert many data at once
+User.insertMany([
+    {name: "doe", email: "doe@gmail.com", age: 15},
+    {name: "joy", email: "joy@gmail.com", age: 52},
+]).then((res) => {
+    console.log(res);
+})
