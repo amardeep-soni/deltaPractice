453e8a8f272da7c6a18df2e274e6ac65bc8fbdd4
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-01-31 11:03:53 +0545

    finding the value from mongodb using mongoose

--- node_with_mongodb/index.js
+++ node_with_mongodb/index.js
@@ -1,18 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-main().then(() => {
+main()
+  .then(() => {
     console.log("Connection to mongodb successs full");
-}).catch(err => console.log(err));
+  })
+  .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
 
 // adding schema
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    age: Number,
+  name: String,
+  email: String,
+  age: Number,
 });
 
 // define model and bind collection with schema (creating a collction in mongodb)
@@ -33,9 +35,42 @@ const User = mongoose.model("User", userSchema);
 // }).catch(err => console.log(err));
 
 // to insert many data at once
-User.insertMany([
-    {name: "doe", email: "doe@gmail.com", age: 15},
-    {name: "joy", email: "joy@gmail.com", age: 52},
-]).then((res) => {
-    console.log(res);
-})
+// User.insertMany([
+//     {name: "doe", email: "doe@gmail.com", age: 15},
+//     {name: "joy", email: "joy@gmail.com", age: 52},
+// ]).then((res) => {
+//     console.log(res);
+// })
+
+// .find() -- it returns an array of matching all filter
+
+// to fetch all data without filter
+// User.find({})
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => console.log(err));
+
+// to fetch all data with filter
+// User.find({age: {$gt : 20}})
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => console.log(err));
+
+// .findOne() -- it returns document of first matched filter
+
+// User.findOne({ _id: "65b8bdd3fa20a80dbc290542" })
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => console.log(err));
+
+// or if we are filtering by id we can do with findOne() but we can also do by method .findById()
+
+// User.findById("65b8bdd3fa20a80dbc290542")
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => console.log(err));
+
