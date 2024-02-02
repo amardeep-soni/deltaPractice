6f216b140ec592f6c89b55e692be7ec4797077dd
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-02-02 14:44:00 +0545

    setup project

---
+++ express_with_mongodb/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const path = require("path");
+
+app.set("viws", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+main()
+  .then(() => {
+    console.log("connection Successfull");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+app.get("/", (req, res) => {
+  res.send("I am working");
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
