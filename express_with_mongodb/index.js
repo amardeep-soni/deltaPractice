6a04375d57d6360d8a1e758c6efeadef65735167
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-02-03 12:32:39 +0545

    index route setup

--- express_with_mongodb/index.js
+++ express_with_mongodb/index.js
@@ -8,6 +8,7 @@ const Chat = require("./models/chat.js");
 app.set("viws", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+
 main()
   .then(() => {
     console.log("connection Successfull");
@@ -17,9 +18,16 @@ main()
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
-
+app.use(express.static(path.join(__dirname, "public")));
+// home route
 app.get("/", (req, res) => {
   res.send("I am working");
+});
+
+// index route
+app.get("/chats", async (req, res) => {
+  let allChats = await Chat.find();
+  res.render("index.ejs", { allChats });
 });
 
 app.listen(3000, () => {
