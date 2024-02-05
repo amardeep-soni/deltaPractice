1bcae2d1874592424f3cc4c6f3e35dda168db468
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-02-05 23:57:12 +0545

    index route showing

--- majorProject/app.js
+++ majorProject/app.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
-const Listing = require("../models/listing.js");
+const Listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1/wonderlust";
 
@@ -15,10 +16,18 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-app.get('/', (req, res) => {
-    res.send("I am root")
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.send("I am root");
 });
 
+//  index route
+app.get("/listings", async (req, res) => {
+  let listings = await Listing.find();
+  res.render("listings/index.ejs", { listings });
+});
 
 app.listen(3000, () => {
   console.log(`Server is listening to port 3000`);
--- majorProject/models/listing.js
+++ majorProject/models/listing.js
@@ -16,7 +16,7 @@ const listingSchema = new Schema({
         ? "https://salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png"
         : v,
   },
-  prica: Number,
+  price: Number,
   location: String,
   country: String,
 });
