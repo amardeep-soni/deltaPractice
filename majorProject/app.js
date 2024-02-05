4ae3b3218beb9dda7636ced59de8a982cc1f29ff
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-02-05 15:13:02 +0545

    saving our first data in the mongobd. concept: default image set

--- majorProject/app.js
+++ majorProject/app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
+const Listing = require("../models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1/wonderlust";
 
@@ -16,6 +17,20 @@ async function main() {
 
 app.get('/', (req, res) => {
     res.send("I am root")
+});
+
+app.get('/testListing', async (req, res) => {
+  let sampleListing = new Listing({
+    title: "Ganesh Villa",
+    description: "By the beach",
+    price: 1200,
+    location: "Calgangute, Goa",
+    country: "India"
+  });
+
+  await sampleListing.save();
+  console.log("Sample was saved");
+  res.send("Successful Testing");
 });
 
 app.listen(3000, () => {
---
+++ majorProject/models/listing.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    reqired: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png",
+    set: (v) =>
+      v === ""
+        ? "https://salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png"
+        : v,
+  },
+  prica: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
