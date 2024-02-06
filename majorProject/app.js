a40dbc149525d909642c45ca64c4faecc6d6eefc
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-02-06 15:12:37 +0545

    we have created layout for ejs templete with ejsMate

--- majorProject/app.js
+++ majorProject/app.js
@@ -4,6 +4,7 @@ var mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1/wonderlust";
 
@@ -21,6 +22,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate)
 
 app.get("/", (req, res) => {
   res.send("I am root");
