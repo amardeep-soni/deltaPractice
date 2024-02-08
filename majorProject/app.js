9690afa745f6d476665d2d584e7dc8eed89a213f
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-02-09 01:31:09 +0545

    server side error handling with wrapAsync and error handling middlewares

--- majorProject/app.js
+++ majorProject/app.js
@@ -5,6 +5,8 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
+const wrapAsync = require("./utils/wrapAsync.js");
 
 const MONGO_URL = "mongodb://127.0.0.1/wonderlust";
 
@@ -19,7 +21,7 @@ async function main() {
 }
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "/views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
@@ -30,52 +32,89 @@ app.get("/", (req, res) => {
 });
 
 //  index route
-app.get("/listings", async (req, res) => {
-  let listings = await Listing.find();
-  res.render("listings/index.ejs", { listings });
-});
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    let listings = await Listing.find();
+    res.render("listings/index.ejs", { listings });
+  })
+);
 
 // new form route
-app.get("/listings/new", async (req, res) => {
-  res.render("listings/new.ejs");
-});
+app.get(
+  "/listings/new",
+  wrapAsync(async (req, res) => {
+    res.render("listings/new.ejs");
+  })
+);
 
 // show route
-app.get("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  let listing = await Listing.findById(id);
-  res.render("listings/show.ejs", { listing });
-});
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+  })
+);
 
 // create route
-app.post("/listings", async (req, res) => {
-  // let { title, description, image, price, location, country } = req.body; -- when we use Listing[name] then we don't need to write this much
+app.post(
+  "/listings",
+  wrapAsync(async (req, res, next) => {
+    // let { title, description, image, price, location, country } = req.body; -- when we use Listing[name] then we don't need to write this much
 
-  // req.body.listing --- it will give object and we pass this object in creating instance of Listing
-  let newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-});
+    // req.body.listing --- it will give object and we pass this object in creating instance of Listing
+    if (!req.body.listing) {
+      next(new ExpressError(400, "Bad Request! Send form data"));
+    }
+    let newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
 
 // edit form route
-app.get("/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  let listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", { listing });
-});
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
 
 // update route
-app.put("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  res.redirect(`/listings/${id}`);
-});
+app.put(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    if (!req.body.listing) {
+      next(new ExpressError(400, "Bad Request! Send form data"));
+    }
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+  })
+);
 
 // delete route
-app.delete("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndDelete(id);
-  res.redirect("/listings");
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+  })
+);
+
+// when no any route matches
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page not found"));
+});
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something error occured" } = err;
+  res.status(statusCode).render("error.ejs", { message });
 });
 
 app.listen(3000, () => {
