3926603a0a0734cd42fbf3f92148a727f9489158
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-02-10 01:57:40 +0545

    one to many approach 2

---
+++ majorProject/relationships/customers.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+main()
+  .then(() => console.log("connecteion successfull"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/realtionDemo");
+}
+
+const orderSchema = new Schema({
+  item: String,
+  price: Number,
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+const addOrders = async () => {
+  let res = await Order.insertMany([
+    { item: "Samosa", price: 12 },
+    { item: "Chips", price: 10 },
+    { item: "Chocolate", price: 40 },
+  ]);
+  console.log(res);
+};
+
+// addOrders();
+
+const customerSchema = new Schema({
+  name: String,
+  orders: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Order",
+    },
+  ],
+});
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+const addCustomer = async () => {
+  let cust1 = new Customer({
+    name: "Rahul Kumar",
+  });
+  let order1 = await Order.findOne({ item: "Chips" });
+  let order2 = await Order.findOne({ item: "Chocolate" });
+
+  cust1.orders.push(order1);
+  cust1.orders.push(order2);
+
+  let result = await cust1.save();
+  console.log(result);
+};
+
+// addCustomer();\
+
+
+const findCustomer = async () => {
+  let cust = await Customer.findOne({ name: "Rahul Kumar" });
+  console.log(cust);
+};
+// findCustomer();
+
+const findCustomerWithOrders = async () => {
+//   let cust = await Customer.findOne({ name: "Rahul Kumar" }).populate("orders");
+  let cust = await Customer.findOne({ name: "Rahul Kumar" }).populate("orders", "item");
+  console.log(cust);
+};
+findCustomerWithOrders();
