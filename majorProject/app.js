955f4114a045c789e1aedaf1959babc752941fa5
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-02-24 20:52:10 +0545

    showing review comment and rating

--- majorProject/app.js
+++ majorProject/app.js
@@ -76,7 +76,7 @@ app.get(
   "/listings/:id",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
-    let listing = await Listing.findById(id);
+    let listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", { listing });
   })
 );
