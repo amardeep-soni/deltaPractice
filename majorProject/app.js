e211a275ec44bd124608abbcb810c5453dea3897
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-02-26 13:18:46 +0545

    deleting review and object id present in listing when delteing review

--- majorProject/app.js
+++ majorProject/app.js
@@ -151,7 +151,7 @@ app.delete(
 );
 
 // Reviews
-// Post Route
+// Review Post Route
 app.post(
   "/listings/:id/reviews",
   validateReview,
@@ -164,6 +164,19 @@ app.post(
     await listing.save();
 
     res.redirect(`/listings/${listing._id}`);
+  })
+);
+
+// Review Delete Route
+app.delete(
+  "/listings/:id/reviews/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    
+    res.redirect(`/listings/${id}`);
   })
 );
 
