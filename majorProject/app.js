33442afe4821885f637ed58ddb3b5e8c54662d42
Amardeep Kumar Soni <amardeepmunni@gmail.com>
2024-02-29 13:34:21 +0545

    setting session in project

--- majorProject/app.js
+++ majorProject/app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
 
 const listings = require("./routes/listings.js");
 const reviews = require("./routes/reviews.js");
@@ -27,6 +28,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+
+const sessionOptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    // expires in milliseconds after 7 days from now
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
+
+app.use(session(sessionOptions));
 
 app.get("/", (req, res) => {
   res.send("I am root");
